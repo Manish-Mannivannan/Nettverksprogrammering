62053ff93298385ce969125457f55a081ca6e48c
Manish Mannivannan <manishma@stud.ntnu.no>
2023-03-03 14:37:07 +0100

    oving_4

--- oving_4/Oppgave_1/Client/target/debug/deps/client.d
+++ oving_4/Oppgave_1/Client/target/debug/deps/client.d
@@ -1,5 +1,5 @@
-C:\Users\manis\Documents\Nettverksprogrammering\oving_4\Oppgave_1_2\Client\target\debug\deps\client.exe: src\main.rs
+C:\Users\manis\Documents\Nettverksprogrammering\oving_4\Oppgave_1\Client\target\debug\deps\client.exe: src\main.rs
 
-C:\Users\manis\Documents\Nettverksprogrammering\oving_4\Oppgave_1_2\Client\target\debug\deps\client.d: src\main.rs
+C:\Users\manis\Documents\Nettverksprogrammering\oving_4\Oppgave_1\Client\target\debug\deps\client.d: src\main.rs
 
 src\main.rs:
